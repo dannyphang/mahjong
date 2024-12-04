6ae50411bf204cc5e4169ea4a5c69df5147a91f4
DannyPhang <dannyphang64@gmail.com>
2024-12-04 09:39:43 +0800

    remove old server folder

--- server/shared/function.js
+++
@@ -1,9 +0,0 @@
-function responseModel({ data = null, isSuccess = true, responseMessage = null }) {
-  return {
-    data: data,
-    isSuccess: isSuccess,
-    responseMessage: responseMessage,
-  };
-}
-
-export default responseModel;
