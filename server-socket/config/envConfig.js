e341f94dc8919db4ccddb8966423fd0ba05ef1a0
DannyPhang <dannyphang64@gmail.com>
2025-02-05 14:57:42 +0800

    update production base URL and set environment to production

--- server-socket/config/envConfig.js
+++ server-socket/config/envConfig.js
@@ -1,7 +1,7 @@
 import env from "./environment.json" assert { type: "json" };
 import envProd from "./environment.prod.json" assert { type: "json" };
 
-let isDev = true;
+let isDev = false;
 
 const port = isDev ? env.port : envProd.port;
 
